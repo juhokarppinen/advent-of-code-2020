3c28d74b7d1f8eebf225d02f9449a51759bfdd1a
Juho Karppinen <juho@juhokarppinen.fi>
2020-12-08 09:29:17 +0200

    add main view

---
+++ src/Main.elm
@@ -0,0 +1,47 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, h1, h2, li, ol, p, span, text, ul)
+import Html.Attributes exposing (style)
+
+
+type alias Solution =
+    { heading : String
+    , solutions : List String
+    }
+
+
+allSolutions : List Solution
+allSolutions =
+    []
+
+
+
+main : Html message
+main =
+    div
+        [ style "display" "flex"
+        , style "flex-direction" "column"
+        , style "align-items" "center"
+        ]
+        (title :: List.map viewDay allSolutions)
+
+
+title : Html message
+title =
+    h1 [] [ text "Advent of Code 2020 – Year of Elm" ]
+
+
+viewDay : Solution -> Html message
+viewDay { heading, solutions } =
+    div
+        [ style "min-width" "25rem"
+        , style "border-bottom" "1px solid #1a1a1a"
+        ]
+        [ h2 [] [ text heading ]
+        , ol [] (List.map viewSolution solutions)
+        ]
+
+
+viewSolution : String -> Html msg
+viewSolution solution =
+    li [] [ text solution ]
